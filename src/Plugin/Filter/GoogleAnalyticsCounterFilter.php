c60c4a5e412363c0729923cf18eb59c6feb71294
Eric Sod <sode@mskcc.org>
2018-02-11 20:38:37 -0500

    Add dependency ingjection to filter.

--- src/Plugin/Filter/GoogleAnalyticsCounterFilter.php
+++ src/Plugin/Filter/GoogleAnalyticsCounterFilter.php
@@ -2,10 +2,13 @@
 
 namespace Drupal\google_analytics_counter\Plugin\Filter;
 
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Path\CurrentPathStack;
+use Drupal\Component\Utility\SafeMarkup;
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
-use Drupal\Component\Utility\SafeMarkup;
 use Drupal\google_analytics_counter\GoogleAnalyticsCounterManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Add filter to show google analytics counter number.
@@ -13,11 +16,59 @@ use Drupal\google_analytics_counter\GoogleAnalyticsCounterManager;
  * @Filter(
  *   id = "google_analytics_counter_filter",
  *   title = @Translation("Google Analytics Counter tag"),
- *   description = @Translation("Substitutes a special Google Analytics Counter tag [gac|...] with the actual content."),
+ *   description = @Translation("Substitutes a special Google Analytics Counter token [gac|/my-path] which prints the pageview count."),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
  * )
  */
-class GoogleAnalyticsCounterFilter extends FilterBase {
+class GoogleAnalyticsCounterFilter extends FilterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current path.
+   *
+   * @var \Drupal\Core\Path\CurrentPathStack
+   */
+  protected $currentPath;
+
+  /**
+   * Drupal\google_analytics_counter\GoogleAnalyticsCounterCommon definition.
+   *
+   * @var \Drupal\google_analytics_counter\GoogleAnalyticsCounterManager
+   */
+  protected $manager;
+
+  /**
+   * Constructs a new SiteMaintenanceModeForm.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Path\CurrentPathStack $current_path
+   *   The current path.
+   * @param \Drupal\google_analytics_counter\GoogleAnalyticsCounterManager $manager
+   *   Google Analytics Counter Manager object.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentPathStack $current_path, GoogleAnalyticsCounterManager $manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->currentPath = $current_path;
+    $this->manager = $manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('path.current'),
+      $container->get('google_analytics_counter.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -28,16 +79,16 @@ class GoogleAnalyticsCounterFilter extends FilterBase {
   }
 
   /**
-   * Finds [gac|...] tags and replaces them by actual values.
+   * Finds [gac|path/to/page] tags and replaces them by actual values.
    */
-  private function handleText($str) {
+  private function handleText($string) {
     // [gac|path/to/page].
     $matchlink = '';
     $orig_match = '';
     $matches = '';
     // This allows more than one pipe sign (|) ...
     // does not hurt and leaves room for possible extension.
-    preg_match_all("/(\[)gac[^\]]*(\])/s", $str, $matches);
+    preg_match_all("/(\[)gac[^\]]*(\])/s", $string, $matches);
 
     foreach ($matches[0] as $match) {
       // Keep original value.
@@ -52,14 +103,14 @@ class GoogleAnalyticsCounterFilter extends FilterBase {
       $path = trim(SafeMarkup::checkPlain(@$match[1]));
 
       // So now we can display the count based on the path.
-      // If no path was defined, the function will detect the current
-      // page's count.
-      // TODO: use dependency injection.
-      $matchlink[] = \Drupal::service('google_analytics_counter.manager')->displayGaCount($path);
+      // If no path was defined, the function will detect the
+      // current node's count.
+      // Todo: make pageview totals and pagepaths equal to avoid confusion.
+      $matchlink[] = $this->manager->displayGaCount($this->currentPath->getPath());
     }
 
-    $str = str_replace($orig_match, $matchlink, $str);
-    return $str;
+    $string = str_replace($orig_match, $matchlink, $string);
+    return $string;
   }
 
 }
